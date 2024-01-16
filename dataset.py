ed61d93ade908abded99d99570f62d18e833231d
OrionZJ <hcmcj720@163.com>
2024-01-16 17:14:43 +0800

    init dataset

---
+++ dataset.py
@@ -0,0 +1,60 @@
+import sys
+
+import numpy as np
+import SimpleITK as sitk
+from torch.utils.data import Dataset
+
+
+def construct_coordinate(sample_points_num, view):
+    view = np.deg2rad(view)
+    x = np.linspace(-1, 1, sample_points_num, endpoint=False)
+    y = np.linspace(-1, 1, sample_points_num, endpoint=False)
+    X, Y = np.meshgrid(x, y, indexing='ij')  # 注意indexing方式 -------> 方向
+    rotation_matrix = np.array([[np.cos(view), -np.sin(view)],
+                               [np.sin(view), np.cos(view)]])
+
+    # 使用numpy的向量化操作提取每一个坐标点对
+    coords = np.column_stack((X.ravel(), Y.ravel())).T
+    new_coords = np.matmul(rotation_matrix, coords)
+    new_coords = new_coords.T.reshape(sample_points_num, sample_points_num, 2)
+    return new_coords
+
+
+class SinogramDataset(Dataset):
+    def __init__(self, sample_points_num, views, sample_N=10, sino_path=None, train=True):
+        if sino_path is None and train is True:
+            print("Sinogram path not given!")
+            sys.exit(1)
+        self.train = train
+        self.sinogram = sitk.GetArrayFromImage(sitk.ReadImage(sino_path))
+        self.sample_points_num = sample_points_num
+        self.views = views
+        self.sample_N = sample_N
+        rays = []
+        intensities = []
+        for i in range(views):
+            rays.append(construct_coordinate(sample_points_num, i))
+            intensities.append(self.sinogram[i, :])
+        self.rays = np.array(rays)
+        self.intensities = np.array(intensities)
+
+    def __len__(self):
+        return len(self.rays)
+
+    def __getitem__(self, index):
+        ray = self.rays[index]
+        intensity = self.intensities[index]
+        if self.train:
+            sample_idx = np.random.choice(len(ray), size=self.sample_N, replace=False)
+            ray_sample = ray[sample_idx]
+            intensity_sample = intensity[sample_idx]
+            return ray_sample, intensity_sample
+        else:
+            return ray, intensity
+
+
+
+
+if __name__ == '__main__':
+    path = "../SCOPE/data/gt_sino.nii"
+    train_data = SinogramDataset(sample_points_num=367, views=60, sino_path=path)
